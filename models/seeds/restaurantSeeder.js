e507cc2050e304fb55f9d8e636d924886d34ea10
Ahno Chen <ahnochen1029@gmail.com>
2020-09-12 00:04:34 +0800

    modify mongoose config

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,11 +1,6 @@
-const mongoose = require('mongoose')
 const restaurantData = require('./restaurant.json')
 const Restaurant = require('../restaurant')
-mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+
 db.once('open', () => {
   console.log('mongodb connected!')
   restaurantData.results.forEach(data => {
