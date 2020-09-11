567418c83401a3d0c17f6e69542b5b703aafae0e
Ahno Chen <ahnochen1029@gmail.com>
2020-09-11 17:16:53 +0800

    modify express routes

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express')
 const mongoose = require('mongoose')
 const exphds = require('express-handlebars')
 const methodOverride = require('method-override')
-const app = express()
+
 const restaurantList = require('./models/seeds/restaurant.json')
 const port = 3000
 const bodyParser = require('body-parser')
 const Restaurant = require('./models/restaurant')
 
+const routes = require('./routes')
+
+const app = express()
 mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
@@ -25,13 +28,7 @@ app.engine('handlebars', exphds({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(methodOverride('_method'))
-
-app.get('/', (req, res) => {
-  Restaurant.find()
-    .lean()
-    .then(restaurant => res.render('index', { restaurant }))
-    .catch(error => console.error(error))
-})
+app.use(routes)
 
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword.toLocaleLowerCase()
@@ -41,80 +38,10 @@ app.get('/search', (req, res) => {
   res.render('index', { restaurant, keyword })
 })
 
-//show restaurant info
-app.get('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .lean()
-    .then((restaurant) => res.render('show', { restaurant }))
-    .catch(error => console.log(error))
-})
+// app.get('/restaurant/new', (req, res) => {
+//   return res.render('new')
+// })
 
-//create new restaurant
-app.get('/new', (req, res) => {
-  return res.render('new')
-})
-
-app.post('/restaurants', (req, res) => {
-  const name = req.body.name
-  const name_en = req.body.name_en
-  const category = req.body.category
-  const image = req.body.image
-  const location = req.body.location
-  const phone = req.body.phone
-  const google_map = req.body.google_map
-  const rating = req.body.rating
-  const description = req.body.description
-  return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-
-//edit restaurant info
-app.get('/restaurants/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .lean()
-    .then((restaurant) => res.render('edit', { restaurant }))
-    .catch(error => console.log(error))
-})
-
-app.put('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  const name = req.body.name
-  const name_en = req.body.name_en
-  const category = req.body.category
-  const image = req.body.image
-  const location = req.body.location
-  const phone = req.body.phone
-  const google_map = req.body.google_map
-  const rating = req.body.rating
-  const description = req.body.description
-  return Restaurant.findById(id)
-    .then(restaurant => {
-      restaurant.name = name
-      restaurant.name_en = name_en
-      restaurant.category = category
-      restaurant.image = image
-      restaurant.location = location
-      restaurant.phone = phone
-      restaurant.google_map = google_map
-      restaurant.rating = rating
-      restaurant.description = description
-      return restaurant.save()
-    })
-    .then(() => res.redirect(`/restaurants/${id}`))
-    .catch(error => console.log(error))
-})
-
-//delete selected restaurant
-app.delete('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .then(restaurant => restaurant.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
 
 //setting static files
 app.use(express.static('public'))
