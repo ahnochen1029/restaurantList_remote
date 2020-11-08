a59bd32e74d6b44d8a2d4fd7c5317a9c3d2d0537
Ahno Chen <ahnochen1029@gmail.com>
2020-11-08 10:21:43 +0800

    add message partials

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
-const exphds = require('express-handlebars')
+const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bodyParser = require('body-parser')
 const hbshelpers = require('handlebars-helpers')
+const comparison = hbshelpers.comparison()
 const routes = require('./routes')
 const session = require('express-session')
+const flash = require('connect-flash')
 
 const usePassport = require('./config/passport')
 require('./config/mongoose')
@@ -12,7 +14,7 @@ require('./config/mongoose')
 const app = express()
 const port = 3000
 
-app.engine('handlebars', exphds({ defaultLayout: 'main', helpers: hbshelpers() }))
+app.engine('handlebars', exphbs({ helpers: comparison, defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(session({
@@ -25,15 +27,16 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 usePassport(app)
-
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  res.locals.error = req.flash('error')
   next()
 })
-
 app.use(routes)
-
 //setting static files
 app.use(express.static('public'))
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../../models/user')
 const passport = require('passport')
+const user = require('../../models/user')
 
 //Login
 router.get('/login', (req, res) => {
@@ -11,6 +12,7 @@ router.get('/login', (req, res) => {
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/',
   failureRedirect: '/users/login',
+  failureFlash: true // 開啟錯誤提示
 }))
 
 //Register
@@ -20,31 +22,47 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: 'Email與password欄位必填' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
   User.findOne({ email }).then(user => {
     if (user) {
-      console.log('User is existed')
-      res.render('register', {
+      errors.push({ message: '這個 Email 已經註冊過了。' })
+      return res.render('register', {
+        errors,
         name,
         email,
         password,
         confirmPassword
       })
-    } else {
-      return User.create({
-        name,
-        email,
-        password,
-      })
-        .then(() => res.redirect('/'))
-        .catch(err => console.log(err))
     }
+    return User.create({
+      name,
+      email,
+      password
+    })
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
   })
 })
-
 
 //Logout
 router.get('/logout', (req, res) => {
   req.logOut()
+  req.flash('success_msg', '你已經成功登出。')
   res.redirect('/users/login')
 })
 
